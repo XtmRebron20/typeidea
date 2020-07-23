7f3b1220ca6fc59cb1c16feb6c3e73ef11871507
XtmRebron20 <xuboyang1996@sina.com>
2020-07-23 16:33:02 +0800

    初始化目录

---
+++ typediea/typediea/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for typediea project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+#os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'typediea.settings')
+profile = os.environ.setdefault('TYPEIDEA_PROFILE', 'develop')
+os.environ.setdefault(" DJANGO_SETTINGS_MODULE","typeidea.settings.%s" % profile)
+
+application = get_wsgi_application()
