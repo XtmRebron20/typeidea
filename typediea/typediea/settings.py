b86f969b7b29a9094fbb05e71fb916f48f07e7f4
XtmRebron20 <xuboyang1996@sina.com>
2020-08-13 16:27:57 +0800

    将sqlite3替换为mysql

--- typediea/typediea/settings.py
+++ typediea/typediea/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'typediea.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'typeidea_db',
+        'USER': 'root',
+        'PASSWORD': '123456',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
