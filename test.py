f6b47d86ade1896879d250b04d18e86e7896640d
XtmRebron20 <xuboyang1996@sina.com>
2020-08-19 16:25:26 +0800

    部署版本—_1

---
+++ test.py
@@ -0,0 +1,21 @@
+import time 
+CACHE = {}
+
+def query(sql):
+    try:
+        result = CACHE[sql]
+    except KeyError:
+        time.sleep(1)
+        result = 'execute %s' %sql
+        CHACHE[sql] = result
+    return result
+
+if __name__ == '__main__':
+    start = time.time()
+    query('SELECT * FROM blog_post')
+    print(time.time() - start)
+
+    start = time.time()
+    query('SELECT * FROM blog_post')
+    print(time.time() - start)
+
